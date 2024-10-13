5ed8d4cc89d1e372966c94fc50476309afbb19bf
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-10-13 08:37:02 +0500

    Add users with roles

--- src/main/java/com/hstn/crud_rest/security/MySecurityConfig.java
+++ src/main/java/com/hstn/crud_rest/security/MySecurityConfig.java
@@ -2,13 +2,18 @@ package com.hstn.crud_rest.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class MySecurityConfig {
 
+	//todo_ crete users, test:test from app.props won't work
 	@Bean
 	public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -31,5 +36,22 @@ public class MySecurityConfig {
 						.build();
 
 		return new InMemoryUserDetailsManager(user1, user2, user3);
+	}
+
+	//todo_ user roles
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(configer -> configer
+		  .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+	  	.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+	  	.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+	  	.requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+	  	.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+		);
+
+		http.httpBasic(Customizer.withDefaults());
+		http.csrf(csfr -> csfr.disable());
+
+		return http.build();
 	}
 }
