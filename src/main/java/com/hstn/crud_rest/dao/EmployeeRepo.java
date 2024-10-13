3898d6508ef7247f39dd748e2a36d42ea55e9d0e
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-10-13 05:31:15 +0500

    Add repoRestResource

--- src/main/java/com/hstn/crud_rest/dao/EmployeeRepo.java
+++ src/main/java/com/hstn/crud_rest/dao/EmployeeRepo.java
@@ -2,7 +2,9 @@ package com.hstn.crud_rest.dao;
 
 import com.hstn.crud_rest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "emps")
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
 
 }
