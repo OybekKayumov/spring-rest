cb0c2d3630f58a21db88250677d193a91cf24176
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-11-10 08:33:44 +0500

    Add employee controller

---
+++ src/main/java/com/hstn/crud_rest/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.hstn.crud_rest.controller;
+
+import com.hstn.crud_rest.entity.Employee;
+import com.hstn.crud_rest.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@GetMapping("/list")
+	public String listEmployees(Model model) {
+		List<Employee> employees = employeeService.findAll();
+		model.addAttribute("employees", employees);
+
+		return "list-employees";
+	}
+}
