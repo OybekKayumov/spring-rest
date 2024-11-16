a1208f013330b0da2803e46c61694fa860ed77ea
Oybek Kayumov <oybek.kayumov@gmail.com>
2024-11-16 15:06:25 +0500

    Add save new employee

--- src/main/java/com/hstn/crud_rest/controller/EmployeeController.java
+++ src/main/java/com/hstn/crud_rest/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.hstn.crud_rest.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -32,5 +34,12 @@ public class EmployeeController {
 		Employee employee = new Employee();
 		model.addAttribute("employee", employee);
 		return "new-employee-form";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.save(employee);
+
+		return "redirect:/employees/list";
 	}
 }
